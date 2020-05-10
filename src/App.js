14ea3364644090f2e1e20ebee231883b49c2f461
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2020-05-09 23:55:22 -0700

    practicing createElement - cool!

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>My 2020 React Kingdom</h2>
-        </div>
-        <p className="App-intro">
-          Muahahahaha!
-        </p>
-      </div>
-    );
+    //return (
+    //  <div className="App">
+    //    <div className="App-header">
+    //      <img src={logo} className="App-logo" alt="logo" />
+    //      <h2>My 2020 React Kingdom</h2>
+    //    </div>
+    //    <p className="App-intro">
+    //      Muahahahaha!
+    //    </p>
+    //  </div>
+    //);
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hello World"))
   }
 }
 

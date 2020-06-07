d75e9eef4cdb9f6d4adacfd08afe5d7653ecd826
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2020-06-07 00:44:32 -0700

    added a brand new component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    //return (
-    //  <div className="App">
-    //    <div className="App-header">
-    //      <img src={logo} className="App-logo" alt="logo" />
-    //      <h2>My 2020 React Kingdom</h2>
-    //    </div>
-    //    <p className="App-intro">
-    //      Muahahahaha!
-    //    </p>
-    //  </div>
-    //);
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hello World"))
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>My 2020 React Kingdom</h2>
+        </div>
+        <p className="App-intro">
+          Muahahahaha!
+        </p>
+
+        <Person />
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hello World"))
   }
 }
 

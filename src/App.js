555bdaf64a0ab33e0b2d1dae82759b954f8f0f88
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2020-08-08 21:54:40 -0700

    added onClick

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    console.log("Was clicked!")
+  }
+
   render() {
     return (
       <div className="App">
@@ -22,7 +26,7 @@ class App extends Component {
         <p className="App-intro">
           Muahahahaha!
         </p>
-        <button>Switch name</button>
+        <button onClick={this.switchNameHandler}>Switch name</button>
 
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >Hobbies: racing</Person>

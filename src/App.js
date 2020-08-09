544db1873e70885c2e50fbee96f1863031dba2eb
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2020-08-08 21:07:14 -0700

    using state instead of hard-coding

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Teng", age: 39},
+      {name: "Zurab", age: 38},
+      {name: "Sonya", age: 36}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,10 +22,11 @@ class App extends Component {
         <p className="App-intro">
           Muahahahaha!
         </p>
+        <button>Switch name</button>
 
-        <Person name="Sonya" age="35" />
-        <Person name="Zurab" age="38" >Hobbies: racing</Person>
-        <Person name="Max" age="40" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >Hobbies: racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hello World"))

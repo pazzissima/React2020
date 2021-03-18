a2e1d825ab5e0ebca229457c5145006068968cd2
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-03-17 20:43:03 -0700

    added styling

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 const userInput = (props) => {
+	const style = {
+		border: '2px solid red'
+	}
   return <input 
   	type="text" 
+  	style={style}
   	onChange={props.changed}
   	value={props.currentName}
   	/>

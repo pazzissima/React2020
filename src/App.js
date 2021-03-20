0ac78e28066d25ef328091b3ba321a7f2e01b5a0
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-03-19 23:25:49 -0700

    deletePersonHandler

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ class App extends Component {
     this.setState({showPersons: !doesShow})
   }
 
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -33,8 +39,11 @@ class App extends Component {
     if(this.state.showPersons){
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age}/>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age} />
           })}
         </div>
       )

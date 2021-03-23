0a7b329c73c57b109ffb1c932b06d27a00a9c6c0
Sofya <sofya_nagaslaeva@gap.com>
2021-03-23 13:41:32 -0700

    flexible lists

--- src/App.js
+++ src/App.js
@@ -20,6 +20,24 @@ class App extends Component {
     this.setState({showPersons: !doesShow})
   }
 
+  nameChangedHandler = (event, id) => {
+      const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {
+      persons: persons
+    })
+  }
+
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
@@ -45,7 +63,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age} 
-              key={person.id}/>
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       )

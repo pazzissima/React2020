2715482800c12428343afa993f77598423d678aa
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-03-26 20:33:48 -0700

    installed Radium for pseudo selectors and media queries, used hover selector

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 
@@ -51,7 +52,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '4px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -70,6 +75,10 @@ class App extends Component {
       )
 
       style.backgroundColor = "red";
+      style[':hover']: {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     };
 
     let classes = [];
@@ -106,4 +115,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);

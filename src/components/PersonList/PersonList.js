858889022b0e9b6fce8cd792f3b5f718919ccf19
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-04-03 09:48:27 -0700

    rewrote PersonList component to make it class-based

--- src/components/PersonList/PersonList.js
+++ src/components/PersonList/PersonList.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 
-const personList = (props) => {
-    console.log('[PersonList.js] rendering...');
-	return props.persons.map((person, index) => {
-        return (
-          <Person
-            click={() => props.clicked(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={event => props.changed(event, person.id)}
-          />
-        );
-    })};
+class PersonList extends Component {
+    render() {
+        console.log('[PersonList.js] rendering...');
+            return this.props.persons.map((person, index) => {
+                return (
+                  <Person
+                    click={() => this.props.clicked(index)}
+                    name={person.name}
+                    age={person.age}
+                    key={person.id}
+                    changed={event => this.props.changed(event, person.id)}
+                  />
+                );
+            }
+        )
+    }
+};
 
-export default personList;
+export default PersonList;

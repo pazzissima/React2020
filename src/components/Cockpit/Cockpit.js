0c50a69daf6dab7b98655fb594043ff1bb5386db
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-04-09 17:06:55 -0700

    Cockpit.js cleanup work in useEffect after Cockpit component removed

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,8 +6,11 @@ const cockpit = (props) => {
 		console.log('[Cockpit.js] useEffect');
 		//HTTP request...
 		setTimeout(() => {
-			alert('Saved data to cloud!');
+			// alert('Saved data to cloud!');
 		}, 1000);
+		return () => {
+			console.log('[Cockpit.js] cleanup work in useEffect');
+		};
 	}, []);
 
 	const assignedClasses = [];

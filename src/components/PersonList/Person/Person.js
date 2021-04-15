ef34fe1dc82dae3141dad079bdb7f42128c466a7
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-04-14 21:03:20 -0700

    using array to render adjacent JSX elements

--- src/components/PersonList/Person/Person.js
+++ src/components/PersonList/Person/Person.js
@@ -5,15 +5,13 @@ import classes from './Person.css';
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
-    return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
-          I'm {this.props.name} and I am {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
-    );
+    return [
+      <p onClick={this.props.click}>
+        I'm {this.props.name} and I am {this.props.age} years old!
+      </p>,
+      <p>{this.props.children}</p>,
+      <input type="text" onChange={this.props.changed} value={this.props.name} />
+    ];
   }
 };
 

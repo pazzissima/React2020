6edbb4d07662e100381d029daf68cdde25dc4333
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-04-14 21:43:46 -0700

    used Fragment - built in component that does the same thing as Auxiliary component

--- src/components/PersonList/Person/Person.js
+++ src/components/PersonList/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
 import classes from './Person.css';
 
@@ -6,13 +6,13 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <Auxiliary>
+      <Fragment>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
-        </p>,
-        <p>{this.props.children}</p>,
+        </p>
+        <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </Auxiliary>
+      </Fragment>
     );
   }
 };

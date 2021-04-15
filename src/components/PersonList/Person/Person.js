c99a262ec3861b67ade90053a5cfa73a7d29a7f1
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-04-14 21:38:26 -0700

    wrapping JSX content into an empty auxiliary component

--- src/components/PersonList/Person/Person.js
+++ src/components/PersonList/Person/Person.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-
+import Auxiliary from '../../../hoc/Auxiliary';
 import classes from './Person.css';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
-    return [
-      <p onClick={this.props.click}>
-        I'm {this.props.name} and I am {this.props.age} years old!
-      </p>,
-      <p>{this.props.children}</p>,
-      <input type="text" onChange={this.props.changed} value={this.props.name} />
-    ];
+    return (
+      <Auxiliary>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>,
+        <p>{this.props.children}</p>,
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </Auxiliary>
+    );
   }
 };
 

976e15a158b8347744d084782f3e1b24274484d2
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-04-16 22:11:07 -0700

    using createRef method as another way to create a ref

--- src/components/PersonList/Person/Person.js
+++ src/components/PersonList/Person/Person.js
@@ -5,8 +5,14 @@ import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.inputElement.focus();
+    // 
+    this.inputElementRef.current.focus();
   }
 
   render() {
@@ -19,9 +25,10 @@ class Person extends Component {
         <p>{this.props.children}</p>
         <input 
           type="text" 
-          ref={(inputEl) => {
-            this.inputElement = inputEl
-          }}
+          // ref={(inputEl) => {
+          //   this.inputElement = inputEl
+          // }}
+          ref={this.inputElementRef}
           onChange={this.props.changed} 
           value={this.props.name} 
         />
